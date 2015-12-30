99463bab4130296ad337006db69f32dca0e6a3fe
Andrew Kanieski <andrew.legacy@gmail.com>
2015-12-30 18:30:09 -0500

    Adjusted CORS middleware

--- app/controllers/system_controller.js
+++ app/controllers/system_controller.js
@@ -4,7 +4,7 @@ module.exports = function SystemController(app) {
     var Controller = this;
 
 
-    app.use(function corsHandler(req, res, next) {
+    app.use("*", function corsHandler(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
